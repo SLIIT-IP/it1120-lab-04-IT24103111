7141fcd598c9fc2d1aadd28362b9963e1ad5d510
IT24103111 <IT24103111@my.sliit.lk>
2024-08-19 18:15:50 +0700

    Add files via upload
    IT24103111Lab4Q2

---
+++ Q2/IT24103111Lab4Q2.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+	public class IT24103111Lab4Q2{
+	public static void main(String[]args){
+	Scanner input=new Scanner(System.in);
+	
+	
+	System.out.print("Please enter exam marks(out of 100) :");
+	double examMarks =input.nextDouble();
+	
+	if(examMarks<0||examMarks>100){
+		System.out.println("Invalid input for exam marks.Terminating program.");
+		return;
+	}
+	System.out.print("Please enter lab submission marks(out of 100) :");
+	double labMarks=input.nextDouble();
+
+	if(labMarks<0|| labMarks>100){
+		System.out.println("Invalid input for lab submission marks.Terminating program.");
+	return;
+	}
+	System.out.print("Please enter the percentage given for the exam :");
+	double examPercentage=input.nextDouble();
+
+	System.out.print("Please enter the percentage given for the lab submission :");
+	double labPercentage=input.nextDouble();
+
+	if(examPercentage+labPercentage!=100){
+		System.out.println("The percentages must add up to 100.Terminating program.");
+	return;
+	}
+	double finalMark=(examMarks*examPercentage/100)+(labMarks*labPercentage/100);
+
+	System.out.println("Final Exam Mark is :"+finalMark);
+	}
+}
+
+
+
+	
+	
+
+
+	
